5cedf51bd45f1bc6f1bae6026424197e5eba9911
Y24 <1767230254@qq.com>
2022-04-18 00:02:00 +0800

    Finish prototype  of DisruptionPage.

---
+++ src/containers/DisruptionPage/components/follower_line_chart.jsx
@@ -0,0 +1,78 @@
+/* eslint-disable no-unused-vars */
+import React, { PureComponent } from 'react';
+import {
+  LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer,
+} from 'recharts';
+
+const data = [
+  {
+    name: 'Sat',
+    uv: 4000,
+    followers: 5,
+    amt: 2400,
+  },
+  {
+    name: 'Sum',
+    uv: 3000,
+    followers: 10,
+    amt: 2210,
+  },
+  {
+    name: 'Mon',
+    uv: 2000,
+    followers: 9,
+    amt: 2290,
+  },
+  {
+    name: 'Tues',
+    uv: 2780,
+    followers: 13,
+    amt: 2000,
+  },
+  {
+    name: 'Wed',
+    uv: 1890,
+    followers: 12,
+    amt: 2181,
+  },
+  {
+    name: 'Thur',
+    uv: 2390,
+    followers: 15,
+    amt: 2500,
+  },
+  {
+    name: 'Fri',
+    uv: 3490,
+    followers: 18,
+    amt: 2100,
+  },
+];
+
+export default class FollowerLineChart extends PureComponent {
+    static demoUrl = 'https://codesandbox.io/s/simple-line-chart-kec3v';
+
+    render() {
+      return (
+        <ResponsiveContainer>
+          <LineChart
+            width={500}
+            height={100}
+            data={data}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="followers" stroke="#8884d8" activeDot={{ r: 8 }} />
+          </LineChart>
+        </ResponsiveContainer>
+      );
+    }
+}
--- src/containers/DisruptionPage/index.jsx
+++ src/containers/DisruptionPage/index.jsx
@@ -6,10 +6,13 @@ import { useTranslation } from 'react-i18next';
 import {
   CardHeader, Col, Container, Row,
 } from 'reactstrap';
-import ManageSearchIcon from '@mui/icons-material/ManageSearch';
+import EventNoteIcon from '@mui/icons-material/EventNote';
 import { Grid, Box } from '@material-ui/core';
 import FacebookIcon from 'mdi-react/FacebookIcon';
-import { Card, CardContent, Input } from '@mui/material';
+import {
+  Card, CardContent, Input, List, ListItem, Stack,
+} from '@mui/material';
+import Button from '@mui/material/Button';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormHelperText from '@mui/material/FormHelperText';
@@ -17,10 +20,18 @@ import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import InputAdornment from '@mui/material/InputAdornment';
 import TextField from '@mui/material/TextField';
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import EditIcon from '@mui/icons-material/Edit';
+import AttachFileIcon from '@mui/icons-material/AttachFile';
+import CommentIcon from '@mui/icons-material/Comment';
+import VisibilityIcon from '@mui/icons-material/Visibility';
 import { DemoCard, DoubleCard } from './components/infoCard';
 import BasicDateRangePicker from './components/dateRangePicker';
 import VectorMap from '../Maps/VectorMap/index';
 import { LocationOptions, TypeOptions } from './components/custom_selector';
+import FollowerLineChart from './components/follower_line_chart';
+import DemoComposedChart from './components/demo_composed_chart';
 
 const DisruptionPage = () => {
   const { t } = useTranslation('common');
@@ -35,115 +46,161 @@ const DisruptionPage = () => {
     type: 'Oil',
     quantity: '100KG',
   };
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#F2F4F7',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
+  const WhiteItem = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#FFFFFF',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
+  const ExpandedButton = styled(Button)(({ theme }) => ({
+    minWidth: '90%',
+  }));
   return (
     <div>
       <Col>
-        <div style={{ display: 'flex', paddingLeft: '12px', verticalAlign: 'center' }}>
-          <ManageSearchIcon />
+        <div style={{ display: 'flex', paddingLeft: '12px' }}>
+          <EventNoteIcon />
           <div style={{ paddingLeft: '12px', paddingBottom: '12px' }}>
-            <b className="page-title">Form</b>
+            <b className="page-title">Diruption</b>
           </div>
         </div>
-        <div className="form_main_div">
-          <div className="form_caption_div">Set up</div>
-          <p className="form_desc_p">
-            You just need to click each angle to show your preference of Disruptions selection
-          </p>
-          <div style={{ display: 'flex', verticalAlign: 'center', alignItems: 'center' }}>
-            <div style={{ fontSize: 18, flexBasis: '50%', width: '100%' }}>
-              <div className="form_item_div">
-                <div style={{ width: 120 }}><b>Price</b></div>
-                <TextField
-                  label="Lower"
-                  type="number"
-                  id="outlined-start-adornment"
-                  sx={{ m: 1 }}
-                  size="small"
-                  InputProps={{
-                    startAdornment: <InputAdornment position="start">$</InputAdornment>,
-                  }}
-                />
-                <b>—</b>
-                <TextField
-                  label="Upper"
-                  type="number"
-                  id="outlined-start-adornment"
-                  size="small"
-                  sx={{ m: 1 }}
-                  InputProps={{
-                    startAdornment: <InputAdornment position="start">$</InputAdornment>,
-                  }}
-                />
-              </div>
-              <div className="form_item_div">
-                <div style={{ width: 110 }}><b>Delivery</b></div>
-                <TextField
-                  label="Earliest"
-                  type="number"
-                  id="outlined-start-adornment"
-                  sx={{ m: 1 }}
-                  size="small"
-                  InputProps={{
-                    // startAdornment: <InputAdornment position="start">{' '}</InputAdornment>,
-                  }}
-                />
-                <b>—</b>
-                <TextField
-                  label="Latest"
-                  type="number"
-                  id="outlined-start-adornment"
-                  size="small"
-                  sx={{
-                    m: 1,
-                  }}
-                  InputProps={{
-                    // startAdornment: <InputAdornment position="start">{' '}</InputAdornment>,
-                  }}
-                />
-              </div>
-              <div className="form_item_div">
-                <div style={{ width: 120 }}><b>Cost</b></div>
-                <TextField
-                  label="Lower"
-                  type="number"
-                  id="outlined-start-adornment"
-                  sx={{ m: 1 }}
-                  size="small"
-                  InputProps={{
-                    startAdornment: <InputAdornment position="start">$</InputAdornment>,
-                  }}
-                />
-                <b>—</b>
-                <TextField
-                  label="Upper"
-                  type="number"
-                  id="outlined-start-adornment"
-                  size="small"
-                  sx={{
-                    m: 1,
-                  }}
-                  InputProps={{
-                    startAdornment: <InputAdornment position="start">$</InputAdornment>,
-                  }}
-                />
-              </div>
-              <div className="form_item_div">
-                <div style={{ width: 70 }}><b>Location</b></div>
-                <FormControl>
-                  <LocationOptions />
-                </FormControl>
-              </div>
-              <div className="form_item_div">
-                <div style={{ width: 70 }}><b>Type</b></div>
-                <FormControl>
-                  <TypeOptions />
-                </FormControl>
-              </div>
-            </div>
-            <div style={{ flexBasis: '50%', backgroundColor: '#C4C4C4' }}>
-              <div />
-            </div>
-          </div>
+        <Grid container spacing={2} justifyContent="space-evenly">
+          <Grid item xs={6}>
+            <Item>
+              <b className="page-title">Diruption type</b>
+            </Item>
+          </Grid>
+          <Grid item xs={6}>
+            <Item>
+              <b className="page-title">Diruption evaluation</b>
+            </Item>
+          </Grid>
+        </Grid>
+        <div className="disruption_main_div">
+          <Grid container spacing={2} justifyContent="center">
+            <Grid item xs={6}>
+              <Item>
+                <Col>
+                  <div style={{ paddingLeft: '12px', paddingBottom: '12px' }}>
+                    <List>
+                      <ListItem>
+                        <div className="disruption_number_div">01</div>
+                        <Container className="disruption_item_div" />
+                      </ListItem>
+                      <ListItem>
+                        <div className="disruption_number_div">02</div>
+                        <Container className="disruption_item_div" />
+                      </ListItem>
+                      <ListItem>
+                        <div className="disruption_number_div">03</div>
+                        <Container className="disruption_item_div" />
+                      </ListItem>
+                      <ListItem>
+                        <div className="disruption_number_div">04</div>
+                        <Container className="disruption_item_div" />
+                      </ListItem>
+                      <ListItem>
+                        <div className="disruption_number_div">05</div>
+                        <Container className="disruption_item_div" />
+                      </ListItem>
+                      <ListItem>
+                        <div className="disruption_number_div">06</div>
+                        <Container className="disruption_item_div" />
+                      </ListItem>
+                    </List>
+                  </div>
+                </Col>
+              </Item>
+            </Grid>
+            <Grid item xs={6}>
+              <Item>
+                <Col>
+                  <div style={{ paddingLeft: '12px', paddingBottom: '12px' }}>
+                    <Grid container spacing={2}>
+                      <Grid item xs={6}>
+                        <WhiteItem>
+                          <Col>
+                            <div style={{ paddingTop: '10px' }}>
+                              <EditIcon />
+                            </div>
+                            <div style={{ fontSize: 30, paddingTop: '10px' }}>
+                              <b>48</b>
+                            </div>
+                            <p>New Post</p>
+                          </Col>
+                        </WhiteItem>
+                      </Grid>
+                      <Grid item xs={6}>
+                        <WhiteItem>
+                          <Col>
+                            <div style={{ paddingTop: '10px' }}>
+                              <AttachFileIcon />
+                            </div>
+                            <div style={{ fontSize: 30, paddingTop: '10px' }}>
+                              <b>291</b>
+                            </div>
+                            <p>Attached Files</p>
+                          </Col>
+                        </WhiteItem>
+                      </Grid>
+                      <Grid item xs={6}>
+                        <WhiteItem>
+                          <Col>
+                            <div style={{ paddingTop: '10px' }}>
+                              <CommentIcon />
+                            </div>
+                            <div style={{ fontSize: 30, paddingTop: '10px' }}>
+                              <b>481</b>
+                            </div>
+                            <p>Comments</p>
+                          </Col>
+                        </WhiteItem>
+                      </Grid>
+                      <Grid item xs={6}>
+                        <WhiteItem>
+                          <Col>
+                            <div style={{ paddingTop: '10px' }}>
+                              <VisibilityIcon />
+                            </div>
+                            <div style={{ fontSize: 30, paddingTop: '10px' }}>
+                              <b>110</b>
+                            </div>
+                            <p>Total Views</p>
+                          </Col>
+                        </WhiteItem>
+                      </Grid>
+                    </Grid>
+                    <Grid container spacing={2} justifyContent="center">
+                      <Grid item xs={10}>
+                        <WhiteItem>
+                          <div style={{ width: '100%', height: 150 }}>
+                            <FollowerLineChart />
+                          </div>
+                        </WhiteItem>
+                      </Grid>
+                      <Grid item xs={10}>
+                        <WhiteItem>
+                          <div style={{ width: '100%', height: 150 }}>
+                            <DemoComposedChart />
+                          </div>
+                        </WhiteItem>
+                      </Grid>
+                    </Grid>
+                  </div>
+
+                  <ExpandedButton variant="contained">More Information</ExpandedButton>
+                </Col>
+              </Item>
+            </Grid>
+          </Grid>
         </div>
       </Col>
     </div>
